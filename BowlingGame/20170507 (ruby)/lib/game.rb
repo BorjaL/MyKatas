82448693ecd740ccf0040e23e6f1328694724291
BorjaL <borja.fpillado@gmail.com>
2017-05-07 19:37:30 +0200

    Refactor frame_index variable and frame_score method

--- BowlingGame/20170507 (ruby)/lib/game.rb
+++ BowlingGame/20170507 (ruby)/lib/game.rb
@@ -3,6 +3,7 @@ class Game
   def initialize
     @score = 0
     @rolls = []
+    @frame_index = 0
   end
 
   def roll(pins_knowcked_down)
@@ -10,18 +11,16 @@ class Game
   end
 
   def score
-    frame_index = 0
     10.times do
-      frame_score = @rolls[frame_index] + @rolls[frame_index + 1]
-      if @rolls[frame_index] == 10
-        @score += @rolls[frame_index] + @rolls[frame_index + 1] + @rolls[frame_index + 2]
-        frame_index += 1
-      elsif spare?(frame_index)
-        @score += frame_score + @rolls[frame_index + 2]
-        frame_index += 2
+      if @rolls[@frame_index] == 10
+        @score += @rolls[@frame_index] + @rolls[@frame_index + 1] + @rolls[@frame_index + 2]
+        @frame_index += 1
+      elsif spare?
+        @score += frame_score + @rolls[@frame_index + 2]
+        @frame_index += 2
       else
         @score += frame_score
-        frame_index += 2
+        @frame_index += 2
       end
     end
     @score
@@ -29,7 +28,11 @@ class Game
 
   private
 
-  def spare?(frame_index)
-    @rolls[frame_index] + @rolls[frame_index + 1] == 10
+  def spare?
+    @rolls[@frame_index] + @rolls[@frame_index + 1] == 10
+  end
+
+  def frame_score
+    @rolls[@frame_index] + @rolls[@frame_index + 1]
   end
 end
