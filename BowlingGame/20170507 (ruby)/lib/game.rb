a153fcafc7c4770092ced050650920ac84a426b3
BorjaL <borja.fpillado@gmail.com>
2017-05-07 12:17:38 +0200

    First test

---
+++ BowlingGame/20170507 (ruby)/lib/game.rb
@@ -0,0 +1,12 @@
+
+class Game
+  attr_reader :score
+
+  def initialize
+    @score = 0
+  end
+
+  def roll(pins_knowcked_down)
+    pins_knowcked_down
+  end
+end
--- BowlingGame/20170507 (ruby)/spec/bowling_game_spec.rb
+++ BowlingGame/20170507 (ruby)/spec/bowling_game_spec.rb
@@ -3,4 +3,11 @@ require_relative '../lib/game'
 
 describe 'bowling-game-kata' do
   let(:game) { Game.new }
+
+  context 'a simple roll with 0 pins down' do
+    it 'the score must be 0' do
+      game.roll(0)
+      expect(game.score).to eq 0
+    end
+  end
 end
