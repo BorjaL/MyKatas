6b31492286ae752a80e91aafd3016dc2ccbdc916
BorjaL <borja.fpillado@gmail.com>
2017-05-07 13:08:45 +0200

    Refactor to implement all rolls in a game

--- BowlingGame/20170507 (ruby)/lib/game.rb
+++ BowlingGame/20170507 (ruby)/lib/game.rb
@@ -1,12 +1,21 @@
 
 class Game
-  attr_reader :score
-
   def initialize
     @score = 0
+    @rolls = []
   end
 
   def roll(pins_knowcked_down)
-    @score += pins_knowcked_down
+    @rolls << pins_knowcked_down
+  end
+
+  def score
+    frame_index = 0
+    10.times do
+      frame_score = @rolls[frame_index] + @rolls[frame_index + 1]
+      @score += frame_score
+      frame_index += 2
+    end
+    @score
   end
 end
--- BowlingGame/20170507 (ruby)/spec/bowling_game_spec.rb
+++ BowlingGame/20170507 (ruby)/spec/bowling_game_spec.rb
@@ -7,6 +7,9 @@ describe 'bowling-game-kata' do
   context 'a simple roll with 0 pins down' do
     it 'the score must be 0' do
       game.roll(0)
+      19.times do
+        game.roll(0)
+      end
       expect(game.score).to eq 0
     end
   end
@@ -14,6 +17,9 @@ describe 'bowling-game-kata' do
   context 'a simple roll with 5 pins down' do
     it 'the score must be 5' do
       game.roll(5)
+      19.times do
+        game.roll(0)
+      end
       expect(game.score).to eq 5
     end
   end
@@ -22,6 +28,9 @@ describe 'bowling-game-kata' do
     it 'the score must be 10' do
       10.times do
         game.roll(1)
+      end
+      10.times do
+        game.roll(0)
       end
       expect(game.score).to eq 10
     end
