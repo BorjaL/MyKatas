dd238173ff022165de55642a806540f3bb6897fb
BorjaL <borja.fpillado@gmail.com>
2017-05-14 19:53:58 +0200

    Add Frames in the game logic

--- BowlingGame/20170513 (java)/src/main/java/bowlinggame/Game.java
+++ BowlingGame/20170513 (java)/src/main/java/bowlinggame/Game.java
@@ -1,18 +1,36 @@
 package bowlinggame;
 
+import java.util.ArrayList;
+
 public class Game {
 
     private int score;
+    private ArrayList<Frame> frames;
+    private int indexFrame;
 
     public Game() {
         this.score = 0;
+        this.indexFrame = 0;
+        this.frames = new ArrayList<Frame>();
+        for (int i = 0; i < 10; i++) {
+            frames.add(new Frame());
+        }
     }
 
     public void roll(int pinsDown){
-        score += pinsDown;
+        Frame currentFrame = frames.get(indexFrame);
+        currentFrame.addRoll(pinsDown);
+
+        if (currentFrame.isComplete()){
+            indexFrame++;
+        }
     }
 
     public int score(){
+        frames.forEach((frame)->{
+            score += frame.getPinsDown();
+        });
+
         return score;
     }
 }
