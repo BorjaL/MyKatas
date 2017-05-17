e9edd8c429f78a3a200fff20d17d2f4331513df1
BorjaL <borja.fpillado@gmail.com>
2017-05-17 23:20:57 +0200

    Only one number as parameter

--- StringCalculator/20170517 (ruby)/lib/string_calculator.rb
+++ StringCalculator/20170517 (ruby)/lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
   class << self
     def add(numbers)
       return 0 if numbers.empty?
+
+      numbers.to_i
     end
   end
 end
--- StringCalculator/20170517 (ruby)/spec/string_calculator_spec.rb
+++ StringCalculator/20170517 (ruby)/spec/string_calculator_spec.rb
@@ -7,5 +7,11 @@ describe StringCalculator do
         expect(StringCalculator.add('')).to eq 0
       end
     end
+
+    context 'given one number string' do
+      it 'returns the same number' do
+        expect(StringCalculator.add('1')).to eq 1
+      end
+    end
   end
 end
